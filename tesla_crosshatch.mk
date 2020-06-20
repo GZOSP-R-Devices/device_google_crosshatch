3a1e17566d5efa915049f471cc3fa2c48b449dbf
theringmaster <gzroms@hotmail.com>
2020-06-20 19:55:42 -0400

    Blueline: Update build fingerprint to QQ3A.200605.001 both: Use new gapps from blissroms

--- tesla_crosshatch.mk
+++ tesla_crosshatch.mk
@@ -25,4 +25,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 BUILD_FINGERPRINT := "google/crosshatch/crosshatch:10/QQ3A.200605.001/6392402:user/release-keys"
 
 $(call inherit-product-if-exists, vendor/google/crosshatch/crosshatch-vendor.mk)
-$(call inherit-product-if-exists, vendor/pixelgapps/pixel-gapps.mk)
+$(call inherit-product-if-exists, vendor/gapps/gapps.mk)
