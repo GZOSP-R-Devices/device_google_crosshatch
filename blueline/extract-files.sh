razorloves/android_device_google_crosshatch

b1c1: Add proprietary file listing
This is a squash of the following:

Author: Ethan Chen <intervigil@gmail.com>
Date:   Wed Nov 28 21:46:17 2018 -0800

    crosshatch: Add proprietary file listing

    Change-Id: I23de72524c5697ba3f6781c45ef4da804bc728b7

Author: razorloves <razorloves@gmail.com>
Date:   Sat Sep 28 16:04:56 2019 -0500

    crosshatch: Proprietary files updates for Q

    Change-Id: Iff8bff9840859df812aaeef05d9f5e735b14e8a8

Author: razorloves <razorloves@gmail.com>
Date:   Tue Jun 25 00:27:18 2019 -0500

    crosshatch: Fix INITIAL_COPYRIGHT_YEAR

    Change-Id: I1cf8492aa64f285c6811becb5537d35dd4f862c7

Author: razorloves <razorloves@gmail.com>
Date:   Sun May 26 01:56:08 2019 -0500

    b1c1: Setup separate extract scripts

    There are differences between the device's vendor
    images so we need to separate the extract files.

    Change-Id: I2e959f2b9d7db4aa8001ea73cd50ce7ce7dc6320

Author: razorloves <razorloves@gmail.com>
Date:   Tue Dec 24 22:11:22 2019 -0600

    b1c1: Update proprietary files list

    Change-Id: Id6710b6ee2fd2df2d68797a3ed05ab447a95cec0

Author: razorloves <razorloves@gmail.com>
Date:   Fri Jan 10 21:08:53 2020 -0600

    b1c1: Update proprietary files list

     * Add SecureUI blobs
     * Add pixel exclusive/experience sysconfig
     * Add CarrierServices for IMS
     * Remove the vendor.display.config blobs, as they are being built
       now with this fix https://review.lineageos.org/266676

    Change-Id: I8d15654956433225f3eed9c57eb07dc3c6e381cf

Author: razorloves <razorloves@gmail.com>
Date:   Sat Sep 21 19:22:22 2019 -0500

    b1c1: Use CarrierSettings app instead of CarrierConfig

    The CarrierSettings app enables/disables carrier apps based on sim
    inserted, so use it instead of CarrierConfig.

    Change-Id: I163dd615e9d8268c5f94a28ae234a30f74c7e83b

Author: razorloves <razorloves@gmail.com>
Date:   Wed Mar 4 22:06:19 2020 -0600

    b1c1: Update proprietary files for QQ2A.200305.002

    Change-Id: If

blueline/extract-files.sh
@@ -1,0 +1,57 @@
+#!/bin/bash
+#
+# Copyright (C) 2018-2019 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e
+
+VENDOR=google
+DEVICE=blueline
+
+# Load extractutils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+
+LINEAGE_ROOT="$MY_DIR"/../../..
+
+HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
+if [ ! -f "$HELPER" ]; then
+    echo "Unable to find helper script at $HELPER"
+    exit 1
+fi
+. "$HELPER"
+
+if [ $# -eq 0 ]; then
+  SRC=adb
+else
+  if [ $# -eq 1 ]; then
+    SRC=$1
+  else
+    echo "$0: bad number of arguments"
+    echo ""
+    echo "usage: $0 [PATH_TO_EXPANDED_ROM]"
+    echo ""
+    echo "If PATH_TO_EXPANDED_ROM is not specified, blobs will be extracted from"
+    echo "the device using adb pull."
+    exit 1
+  fi
+fi
+
+# Initialize the helper
+setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT"
+
+extract "$MY_DIR"/../crosshatch/"$DEVICE"/device-proprietary-files.txt "$SRC"
+extract "$MY_DIR"/../crosshatch/"$DEVICE"/device-proprietary-files-other.txt "$SRC"
+
+"$MY_DIR"/../crosshatch/"$DEVICE"/setup-makefiles.sh

blueline/setup-makefiles.sh
@@ -1,0 +1,48 @@
+#!/bin/bash
+#
+# Copyright (C) 2017-2019 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e
+
+VENDOR=google
+DEVICE=blueline
+
+INITIAL_COPYRIGHT_YEAR=2018
+
+# Load extractutils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+
+LINEAGE_ROOT="$MY_DIR"/../../../..
+
+HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
+if [ ! -f "$HELPER" ]; then
+    echo "Unable to find helper script at $HELPER"
+    exit 1
+fi
+. "$HELPER"
+
+# Initialize the helper
+setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT"
+
+# Copyright headers and guards
+write_headers
+
+# The standard blobs
+write_makefiles "$MY_DIR"/device-proprietary-files.txt
+write_makefiles "$MY_DIR"/device-proprietary-files-other.txt true
+
+# Finish
+write_footers

crosshatch/extract-files.sh
@@ -1,0 +1,57 @@
+#!/bin/bash
+#
+# Copyright (C) 2018-2019 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e
+
+VENDOR=google
+DEVICE=crosshatch
+
+# Load extractutils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+
+LINEAGE_ROOT="$MY_DIR"/../../..
+
+HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
+if [ ! -f "$HELPER" ]; then
+    echo "Unable to find helper script at $HELPER"
+    exit 1
+fi
+. "$HELPER"
+
+if [ $# -eq 0 ]; then
+  SRC=adb
+else
+  if [ $# -eq 1 ]; then
+    SRC=$1
+  else
+    echo "$0: bad number of arguments"
+    echo ""
+    echo "usage: $0 [PATH_TO_EXPANDED_ROM]"
+    echo ""
+    echo "If PATH_TO_EXPANDED_ROM is not specified, blobs will be extracted from"
+    echo "the device using adb pull."
+    exit 1
+  fi
+fi
+
+# Initialize the helper
+setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT"
+
+extract "$MY_DIR/$DEVICE"/device-proprietary-files.txt "$SRC"
+extract "$MY_DIR/$DEVICE"/device-proprietary-files-other.txt "$SRC"
+
+"$MY_DIR/$DEVICE"/setup-makefiles.sh

crosshatch/setup-makefiles.sh
@@ -1,0 +1,48 @@
+#!/bin/bash
+#
+# Copyright (C) 2017-2019 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e
+
+VENDOR=google
+DEVICE=crosshatch
+
+INITIAL_COPYRIGHT_YEAR=2018
+
+# Load extractutils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+
+LINEAGE_ROOT="$MY_DIR"/../../../..
+
+HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
+if [ ! -f "$HELPER" ]; then
+    echo "Unable to find helper script at $HELPER"
+    exit 1
+fi
+. "$HELPER"
+
+# Initialize the helper
+setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT"
+
+# Copyright headers and guards
+write_headers
+
+# The standard blobs
+write_makefiles "$MY_DIR"/device-proprietary-files.txt
+write_makefiles "$MY_DIR"/device-proprietary-files-other.txt true
+
+# Finish
+write_footers

GZOSP-R-Devices/device_google_crosshatch

blueline/extract-files.sh
@@ -1,0 +1,57 @@
+#!/bin/bash
+#
+# Copyright (C) 2018-2019 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e
+
+VENDOR=google
+DEVICE=blueline
+
+# Load extractutils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+
+GZOSP_ROOT="$MY_DIR"/../../..
+
+HELPER="$LINEAGE_ROOT"/vendor/gzosp/build/tools/extract_utils.sh
+if [ ! -f "$HELPER" ]; then
+    echo "Unable to find helper script at $HELPER"
+    exit 1
+fi
+. "$HELPER"
+
+if [ $# -eq 0 ]; then
+  SRC=adb
+else
+  if [ $# -eq 1 ]; then
+    SRC=$1
+  else
+    echo "$0: bad number of arguments"
+    echo ""
+    echo "usage: $0 [PATH_TO_EXPANDED_ROM]"
+    echo ""
+    echo "If PATH_TO_EXPANDED_ROM is not specified, blobs will be extracted from"
+    echo "the device using adb pull."
+    exit 1
+  fi
+fi
+
+# Initialize the helper
+setup_vendor "$DEVICE" "$VENDOR" "$GZOSP_ROOT"
+
+extract "$MY_DIR"/../crosshatch/"$DEVICE"/device-proprietary-files.txt "$SRC"
+extract "$MY_DIR"/../crosshatch/"$DEVICE"/device-proprietary-files-other.txt "$SRC"
+
+"$MY_DIR"/../crosshatch/"$DEVICE"/setup-makefiles.sh

blueline/setup-makefiles.sh
@@ -1,0 +1,48 @@
+#!/bin/bash
+#
+# Copyright (C) 2017-2019 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e
+
+VENDOR=google
+DEVICE=blueline
+
+INITIAL_COPYRIGHT_YEAR=2018
+
+# Load extractutils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+
+GZOSP_ROOT="$MY_DIR"/../../../..
+
+HELPER="$GZOSP_ROOT"/vendor/gzosp/build/tools/extract_utils.sh
+if [ ! -f "$HELPER" ]; then
+    echo "Unable to find helper script at $HELPER"
+    exit 1
+fi
+. "$HELPER"
+
+# Initialize the helper
+setup_vendor "$DEVICE" "$VENDOR" "$GZOSP_ROOT"
+
+# Copyright headers and guards
+write_headers
+
+# The standard blobs
+write_makefiles "$MY_DIR"/proprietary-files-vendor.txt
+write_makefiles "$MY_DIR"/proprietary-files.txt true
+
+# Finish
+write_footers

crosshatch/extract-files.sh
@@ -1,0 +1,57 @@
+#!/bin/bash
+#
+# Copyright (C) 2018-2019 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e
+
+VENDOR=google
+DEVICE=crosshatch
+
+# Load extractutils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+
+GZOSP_ROOT="$MY_DIR"/../../..
+
+HELPER="$GZOSP_ROOT"/vendor/gzosp/build/tools/extract_utils.sh
+if [ ! -f "$HELPER" ]; then
+    echo "Unable to find helper script at $HELPER"
+    exit 1
+fi
+. "$HELPER"
+
+if [ $# -eq 0 ]; then
+  SRC=adb
+else
+  if [ $# -eq 1 ]; then
+    SRC=$1
+  else
+    echo "$0: bad number of arguments"
+    echo ""
+    echo "usage: $0 [PATH_TO_EXPANDED_ROM]"
+    echo ""
+    echo "If PATH_TO_EXPANDED_ROM is not specified, blobs will be extracted from"
+    echo "the device using adb pull."
+    exit 1
+  fi
+fi
+
+# Initialize the helper
+setup_vendor "$DEVICE" "$VENDOR" "$GZOSP_ROOT"
+
+extract "$MY_DIR/$DEVICE"/device-proprietary-files.txt "$SRC"
+extract "$MY_DIR/$DEVICE"/device-proprietary-files-other.txt "$SRC"
+
+"$MY_DIR/$DEVICE"/setup-makefiles.sh

crosshatch/setup-makefiles.sh
@@ -1,0 +1,48 @@
+#!/bin/bash
+#
+# Copyright (C) 2017-2019 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e
+
+VENDOR=google
+DEVICE=crosshatch
+
+INITIAL_COPYRIGHT_YEAR=2018
+
+# Load extractutils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+
+GZOSP_ROOT="$MY_DIR"/../../../..
+
+HELPER="$GZOSP_ROOT"/vendor/gzosp/build/tools/extract_utils.sh
+if [ ! -f "$HELPER" ]; then
+    echo "Unable to find helper script at $HELPER"
+    exit 1
+fi
+. "$HELPER"
+
+# Initialize the helper
+setup_vendor "$DEVICE" "$VENDOR" "$GZOSP_ROOT"
+
+# Copyright headers and guards
+write_headers
+
+# The standard blobs
+write_makefiles "$MY_DIR"/device-proprietary-files.txt
+write_makefiles "$MY_DIR"/device-proprietary-files-other.txt true
+
+# Finish
+write_footers
dbfc107405cafe9b339d4c3548cec7c2f4f11bc8
Ethan Chen <intervigil@gmail.com>
2018-11-28 21:46:17 -0800

    b1c1: Add proprietary file listing
    This is a squash of the following:
    
    Author: Ethan Chen <intervigil@gmail.com>
    Date:   Wed Nov 28 21:46:17 2018 -0800
    
        crosshatch: Add proprietary file listing
    
        Change-Id: I23de72524c5697ba3f6781c45ef4da804bc728b7
    
    Author: razorloves <razorloves@gmail.com>
    Date:   Sat Sep 28 16:04:56 2019 -0500
    
        crosshatch: Proprietary files updates for Q
    
        Change-Id: Iff8bff9840859df812aaeef05d9f5e735b14e8a8
    
    Author: razorloves <razorloves@gmail.com>
    Date:   Tue Jun 25 00:27:18 2019 -0500
    
        crosshatch: Fix INITIAL_COPYRIGHT_YEAR
    
        Change-Id: I1cf8492aa64f285c6811becb5537d35dd4f862c7
    
    Author: razorloves <razorloves@gmail.com>
    Date:   Sun May 26 01:56:08 2019 -0500
    
        b1c1: Setup separate extract scripts
    
        There are differences between the device's vendor
        images so we need to separate the extract files.
    
        Change-Id: I2e959f2b9d7db4aa8001ea73cd50ce7ce7dc6320
    
    Author: razorloves <razorloves@gmail.com>
    Date:   Tue Dec 24 22:11:22 2019 -0600
    
        b1c1: Update proprietary files list
    
        Change-Id: Id6710b6ee2fd2df2d68797a3ed05ab447a95cec0
    
    Author: razorloves <razorloves@gmail.com>
    Date:   Fri Jan 10 21:08:53 2020 -0600
    
        b1c1: Update proprietary files list
    
         * Add SecureUI blobs
         * Add pixel exclusive/experience sysconfig
         * Add CarrierServices for IMS
         * Remove the vendor.display.config blobs, as they are being built
           now with this fix https://review.lineageos.org/266676
    
        Change-Id: I8d15654956433225f3eed9c57eb07dc3c6e381cf
    
    Author: razorloves <razorloves@gmail.com>
    Date:   Sat Sep 21 19:22:22 2019 -0500
    
        b1c1: Use CarrierSettings app instead of CarrierConfig
    
        The CarrierSettings app enables/disables carrier apps based on sim
        inserted, so use it instead of CarrierConfig.
    
        Change-Id: I163dd615e9d8268c5f94a28ae234a30f74c7e83b
    
    Author: razorloves <razorloves@gmail.com>
    Date:   Wed Mar 4 22:06:19 2020 -0600
    
        b1c1: Update proprietary files for QQ2A.200305.002
    
        Change-Id: If41e8cce1e5af2d5fa25782d21aa8deffdf6bbfa
    
    Author: razorloves <razorloves@gmail.com>
    Date:   Sun Mar 8 00:48:49 2020 -0600
    
        b1c1: Add missing SecureUI libs
    
        The secureui service crashes without these.
    
        Change-Id: I674a32c8d490c743ad824bf78a651639e3ec93ef
    
    Author: razorloves <razorloves@gmail.com>
    Date:   Wed Mar 4 20:06:43 2020 -0600
    
        b1c1: Add needed verizon blobs
    
        Change-Id: I002a2394b1f3991dbde9e9f53e9aebad2871fbcc
    
    Author: razorloves <razorloves@gmail.com>
    Date:   Sun Jun 7 18:57:27 2020 -0500
    
        b1c1: Update proprietary files for QQ3A.200605.001
    
        Change-Id: Ifeda34d28016ad8a172b6991c9a178ebb4f738b4
    
    Author: razorloves <razorloves@gmail.com>
    Date:   Mon Jul 13 12:43:51 2020 -0500
    
        b1c1: Mark obdm apk PRESIGNED for sepolicy domain
    
        This fixes the sepolicy domain on the verizon obdm app.
        The domain is enforced with it's signature in 039e39c,
        so the apk needs to keep that signature.
    
        Change-Id: Ic0189bdecb01e5b2cd90b0e960c216d53aee7567
    
    Author: razorloves <razorloves@gmail.com>
    Date:   Sat Aug 8 15:28:31 2020 -0500
    
        b1c1: Add new codec2 proprietary flies
    
        Google removed the codec2.vendor.ext.policy file from the device repo
        so it needs to be pulled from the factory image now.
        The codec2.vendor.base.policy file is a new blob added in Aug 2020
        factory image, which is needed for vendor.qti.media.c2@1.0-service.
    
        Change-Id: I6a8cdbf3677b31917ad6aa6028ebb2b22d3241ec
    
    Change-Id: Iaeb43fa17645df37d7dde28e15532f844aa8d70c

--- blueline/BoardConfigGzosp.mk
+++ blueline/BoardConfigGzosp.mk
@@ -1,2 +1,4 @@
 # Boardconfig for gzosp_blueline
 -include device/google/crosshatch/BoardConfigGzosp.mk
+
+-include vendor/google/blueline/BoardConfigVendor.mk
--- crosshatch/BoardConfigGzosp.mk
+++ crosshatch/BoardConfigGzosp.mk
@@ -1,2 +1,4 @@
 # Boardconfig for gzosp_crosshatch
 -include device/google/crosshatch/BoardConfigGzosp.mk
+
+-include vendor/google/crosshatch/BoardConfigVendor.mk
