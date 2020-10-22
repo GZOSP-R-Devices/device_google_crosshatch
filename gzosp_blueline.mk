e6d05f94353064ab5ff84a671ab7c003c8cb4e7c
theringmaster <gzroms@hotmail.com>
2020-10-22 13:45:51 -0400

    android-11.0.0_r4
    Android 11.0.0 Release 4 (RP1A.201005.004)
    
    Change-Id: I8bdb0df6f577158281f05ea24891d7f22f9fb43d

--- gzosp_blueline.mk
+++ gzosp_blueline.mk
@@ -19,8 +19,8 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=blueline \
-    PRIVATE_BUILD_DESC="blueline-user 11 RP1A.200720.009 6720564 release-keys"
+    PRIVATE_BUILD_DESC="blueline-user 11 RP1A.201005.004 6782484 release-keys"
 
-BUILD_FINGERPRINT := google/blueline/blueline:11/RP1A.200720.009/6720564:user/release-keys
+BUILD_FINGERPRINT := "google/blueline/blueline:11/RP1A.201005.004/6782484:user/release-keys"
 
 $(call inherit-product-if-exists, vendor/google/blueline/blueline-vendor.mk)
