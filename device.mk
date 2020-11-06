d36257e484fc0c3834d2fc53bc98fd45c367bd89
theringmaster <gzroms@hotmail.com>
2020-11-06 13:58:09 -0500

    Adjust crosshatch for november patches

--- device.mk
+++ device.mk
@@ -264,7 +264,7 @@ PRODUCT_PACKAGES += \
 
 #Vendor Patch Level
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.build.security_patch=2020-10-05
+    ro.vendor.build.security_patch=2020-11-05
 
 # graphics
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -839,7 +839,7 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 
 # Increment the SVN for any official public releases
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.build.svn=38
+    ro.vendor.build.svn=39
 
 #PRODUCT_PRODUCT_PROPERTIES += \
 #    ro.adb.secure=1
